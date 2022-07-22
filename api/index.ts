d85ac66ef2b946f981d6039688766fa67b719b30
iCrawl <buechler.noel@outlook.com>
2022-07-22 17:50:57 +0200

    ci: edge cache

--- api/index.ts
+++ api/index.ts
@@ -8,7 +8,10 @@ export default async function handler(request: VercelRequest, response: VercelRe
 	try {
 		const filePath = join(cwd(), url);
 		const file = await readFile(filePath, 'utf8');
-		response.setHeader('Content-Type', 'application/json').send(file);
+		response
+			.setHeader('Content-Type', 'application/json')
+			.setHeader('Cache-Control', 'max-age=604800, s-maxage=31536000')
+			.send(file);
 	} catch {
 		response.status(404).end();
 	}
