6ee9a240b64cb981272a404fb911474d07d11e43
iCrawl <buechler.noel@outlook.com>
2022-07-22 13:45:44 +0200

    build: vercel serverless function for docs delivery

---
+++ api/index.ts
@@ -0,0 +1,15 @@
+import { readFile } from 'node:fs/promises';
+import { join } from 'node:path';
+import { cwd } from 'node:process';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default async function handler(request: VercelRequest, response: VercelResponse) {
+	const url = request.query.url as string;
+	try {
+		const filePath = join(cwd(), url);
+		const file = await readFile(filePath, 'utf8');
+		response.setHeader('Content-Type', 'application/json').send(file);
+	} catch {
+		response.status(404).end();
+	}
+}
