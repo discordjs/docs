61c9f3bd8e8e9097a45bb1f125d89cfb54f03734
iCrawl <buechler.noel@outlook.com>
2022-08-22 16:19:07 +0200

    feat: info endpoint

---
+++ api/info.ts
@@ -0,0 +1,18 @@
+import { readdir } from 'node:fs/promises';
+import { join } from 'node:path';
+import { cwd } from 'node:process';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default async function handler(request: VercelRequest, response: VercelResponse) {
+	const url = request.query.package as string;
+	try {
+		const filePath = join(cwd(), url);
+		const file = await readdir(filePath, 'utf8');
+		response
+			.setHeader('Content-Type', 'application/json')
+			.setHeader('Cache-Control', 'public, max-age=604800, s-maxage=31536000')
+			.send(file.filter((f) => f.endsWith('.api.json')).map((v) => v.slice(0, -9)));
+	} catch {
+		response.status(404).end();
+	}
+}
